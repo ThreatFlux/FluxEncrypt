d52a98062d706373dcaf0f628a6e57e47da06eaf
github-action-benchmark <github@users.noreply.github.com>
2025-09-06 17:25:35 +0000

    add Benchmark (cargo) benchmark result for a3a1c450f4cbaef9b42fd054ef2cfad00f5e8622

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757179025478,
+  "lastUpdate": 1757179535632,
   "repoUrl": "https://github.com/ThreatFlux/FluxEncrypt",
   "entries": {
     "Benchmark": [
@@ -64,6 +64,72 @@ window.BENCHMARK_DATA = {
           {
             "name": "encryption/hybrid/1048576",
             "value": 60,
+            "range": "± 1",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Wyattroersma@gmail.com",
+            "name": "Wyatt Roersma",
+            "username": "wroersma"
+          },
+          "committer": {
+            "email": "Wyattroersma@gmail.com",
+            "name": "Wyatt Roersma",
+            "username": "wroersma"
+          },
+          "distinct": true,
+          "id": "a3a1c450f4cbaef9b42fd054ef2cfad00f5e8622",
+          "message": "style: Remove trailing whitespace in verify.rs\n\n- Applied cargo fmt to remove trailing whitespace\n- No functional changes",
+          "timestamp": "2025-09-06T13:15:48-04:00",
+          "tree_id": "71c0b5b959d0684847e66ae4b5356bf3a39a861e",
+          "url": "https://github.com/ThreatFlux/FluxEncrypt/commit/a3a1c450f4cbaef9b42fd054ef2cfad00f5e8622"
+        },
+        "date": 1757179535209,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "key_generation/rsa/2048",
+            "value": 210627466,
+            "range": "± 133270340",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "key_generation/rsa/3072",
+            "value": 779220113,
+            "range": "± 545212000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "key_generation/rsa/4096",
+            "value": 2515036500,
+            "range": "± 1675280743",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encryption/hybrid/1024",
+            "value": 236713,
+            "range": "± 377",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encryption/hybrid/8192",
+            "value": 240580,
+            "range": "± 499",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encryption/hybrid/65536",
+            "value": 270560,
+            "range": "± 1128",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encryption/hybrid/1048576",
+            "value": 70,
             "range": "± 1",
             "unit": "ns/iter"
           }
