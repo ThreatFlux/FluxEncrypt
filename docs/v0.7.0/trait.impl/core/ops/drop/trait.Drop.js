e9af5e8b324909607de01292300e78050973b247
wroersma <wroersma@users.noreply.github.com>
2025-12-30 18:58:11 +0000

    deploy: b8f516a3bc5f246d988dd6b59dbc3fa510a11751

---
+++ docs/v0.7.0/trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["fluxencrypt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"fluxencrypt/encryption/aes_gcm/struct.AesKey.html\" title=\"struct fluxencrypt::encryption::aes_gcm::AesKey\">AesKey</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"fluxencrypt/env/secrets/struct.EnvSecret.html\" title=\"struct fluxencrypt::env::secrets::EnvSecret\">EnvSecret</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"fluxencrypt/keys/private_key/struct.PrivateKey.html\" title=\"struct fluxencrypt::keys::private_key::PrivateKey\">PrivateKey</a>"]]],["fluxencrypt_cli",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"fluxencrypt_cli/commands/benchmark/struct.SecureTempFiles.html\" title=\"struct fluxencrypt_cli::commands::benchmark::SecureTempFiles\">SecureTempFiles</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[917,356]}
