3ce698a28eab803ac9a29810341c888969f37e99
wroersma <wroersma@users.noreply.github.com>
2026-08-12 06:22:42 +0000

    deploy: 768251b748cc5f9a85d1e89a2c9bb24685d5ead1

---
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["fluxencrypt_cli",[["impl Subcommand for <a class=\"enum\" href=\"fluxencrypt_cli/commands/config/enum.ConfigAction.html\" title=\"enum fluxencrypt_cli::commands::config::ConfigAction\">ConfigAction</a>",0],["impl Subcommand for <a class=\"enum\" href=\"fluxencrypt_cli/enum.Commands.html\" title=\"enum fluxencrypt_cli::Commands\">Commands</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[351]}
